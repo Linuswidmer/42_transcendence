235a6e184e1b519155d85bda7d2aef4c594b6ab6
Linus Widmer <lwidmer@student.42berlin.de>
2024-03-07 10:22:33 +0100

    working structure of the project

---
+++ Makefile
@@ -0,0 +1,12 @@
+dev:
+	make -C ./srcs/app_server dev
+
+prod:
+	cd srcs && docker compose up --build
+
+down:
+	cd srcs && docker compose down
+
+setup:
+	virtualenv venv && . venv/bin/activate
+	pip install -r ./srcs/requirements/app_server/requirements.txt
---
+++ srcs/app_server/app_server/routing.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application  # Import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+import pong.consumers
+
+websocket_urlpatterns = [
+    path('ws/pong/', pong.consumers.WebSocketConsumer.as_asgi()),
+]
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            websocket_urlpatterns
+        )
+    ),
+})
