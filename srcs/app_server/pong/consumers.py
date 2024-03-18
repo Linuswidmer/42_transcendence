e79c02b6f32a0e969b2701684e774e5a3f36e10f
Linus Widmer <lwidmer@student.42berlin.de>
2024-03-18 14:30:18 +0100

    redis service now working, had to change the port in the env.prod

--- srcs/app_server/pong/consumers.py
+++ srcs/app_server/pong/consumers.py
@@ -1,16 +1,44 @@
 # consumers.py
 from channels.generic.websocket import AsyncWebsocketConsumer
+import json
 
 class WebSocketConsumer(AsyncWebsocketConsumer):
     async def connect(self):
+        # Join a group
+        await self.channel_layer.group_add(
+            "chat",
+            self.channel_name
+        )
+
         # Accept the WebSocket connection
         await self.accept()
-        
-        # Send a "Hello, world!" message to the client
-        await self.send(text_data="Hello, world!")
 
     async def disconnect(self, close_code):
-        pass
+        # Leave the group
+        await self.channel_layer.group_discard(
+            "chat",
+            self.channel_name
+        )
 
+    # Receive message from WebSocket
     async def receive(self, text_data):
-        pass
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+
+        # Send message to group
+        await self.channel_layer.group_send(
+            "chat",
+            {
+                'type': 'chat_message',
+                'message': message
+            }
+        )
+
+    # Receive message from group
+    async def chat_message(self, event):
+        message = event['message']
+
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps({
+            'message': message
+        }))
