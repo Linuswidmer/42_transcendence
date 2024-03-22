cdd5caa1a667124e05dbdd784b736bd37b8ef2ee
Leon <leon.yannik@web.de>
2024-03-22 15:51:15 +0100

    uncomment smth that was needed

--- srcs/app_server/app_server/asgi.py
+++ srcs/app_server/app_server/asgi.py
@@ -5,18 +5,28 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-# from routing import websocket_urlpatterns
+# from app_server.routing import websocket_urlpatterns
+
+from django.urls import path
+from pong.consumers import WebSocketConsumer, PongConsumer
+from pong_online.consumers import MultiplayerConsumer
+
+websocket_urlpatterns = [
+    path('ws/pong/', WebSocketConsumer.as_asgi()),
+    path('ws/pong/game/', PongConsumer.as_asgi()),
+	path('ws/pong_online/game/', MultiplayerConsumer.as_asgi()),
+]
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app_server.settings")
 
 django_asgi_app = get_asgi_application()
 
-#leon: i think we dont need this here, feel free to uncomment again if smth stopped working
-# application = ProtocolTypeRouter(
-#     {
-#         "http": django_asgi_app,
-#         "websocket": AllowedHostsOriginValidator(
-#             AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
-#         ),
-#     }
-# )
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+        ),
+    }
+)
