88c395e01a0f2a4753ec75426ab1dfc4f69990ba
leonyannick <leon.yannik@web.de>
2024-04-11 19:54:37 +0200

    lobby refactor

--- srcs/app_server/pong_online/static/pong_online/lobby.js
+++ srcs/app_server/pong_online/static/pong_online/lobby.js
@@ -1,58 +1,3 @@
-// function setupButtons() {
-// 	//////////////////////// REGISTER BUTTON ///////////////////////
-//     let registerButton = document.querySelector('.register');
-//     registerButton.addEventListener('click', function() {
-//         // Your code here
-//         console.log("Register button clicked");
-//     });
-
-// 	//////////////////////// JOIN BUTTON /////////////////////////
-// 	var joinButtons = document.querySelectorAll('.join');
-//     joinButtons.forEach(function(button) {
-//         button.addEventListener('click', function() {
-//             var match_id = this.getAttribute('data-match-id');
-//             fetch('/join/', {
-//                 method: 'POST',
-//                 headers: {
-//                     'Content-Type': 'application/json',
-//                     // Add your CSRF token here
-//                 },
-//                 body: JSON.stringify({match_id: match_id})
-//             })
-//             .then(response => response.json())
-//             .then(data => {
-//                 if (data.status === 'error') {
-//                     // Display error message
-//                     alert(data.error_message);
-//                 } else {
-//                     // Handle success
-                    
-//                 }
-//             });
-//         });
-//     });
-// }
-
-// document.addEventListener('DOMContentLoaded', (event) => {
-//     let registerButtons = document.querySelectorAll('.register');
-//     registerButtons.forEach((button) => {
-//         button.addEventListener('click', function() {
-//             let match_id = this.getAttribute('data-match-id');
-//             console.log("Register button clicked for match id: ", match_id);
-//             // Your code here...
-//         });
-//     });
-
-//     let joinButtons = document.querySelectorAll('.join');
-//     joinButtons.forEach((button) => {
-//         button.addEventListener('click', function() {
-//             let match_id = this.getAttribute('data-match-id');
-//             console.log("Join button clicked for match id: ", match_id);
-//             // Your code here...
-//         });
-//     });
-// });
-
 
 const protocol = window.location.protocol.match(/^https/) ? 'wss' : 'ws';
     // const wsUrl = protocol + `://${window.location.host}/ws/pong/${roomName}/`; // this has to be modified to be a unique identifier
@@ -66,26 +11,108 @@ const data = document.currentScript.dataset;
 const username = data.username;
 console.log("username from request", username);
 
-ws.onopen = function(e) {
-	// telling the server that the client is ready
-	console.log('WebSocket connection established');
-	// let data = {'playerId': 'SESSION ID HERE?????'};
-	// ws.send(JSON.stringify(data));
-	ws.username = username
-	ws.send(JSON.stringify({type: 'username', 'username': username}));
-	ws.send(JSON.stringify({type: 'lobby_update', 'action': 'display'}));
-};
 
-ws.onmessage = function(e) {
-	try{
-		const data = JSON.parse(e.data);
-		console.log("ws.onmessage:", data);
+class Lobby {
+	constructor(ws, username) {
+        this.ws = ws;
+		this.username = username;
 
-		if (data.type === "lobby_update") {
-			updateLobby(data);
+		this.modus = null;
+
+		this.play_local_button = document.getElementById('create_game');
+		this.play_local_button.addEventListener('click', () => {
+            this.handle_create_game_button_click();
+        });
+
+		this.play_local_button = document.getElementById('play_local');
+		this.play_local_button.addEventListener('click', () => {
+            this.handle_play_local_button_click();
+        });
+
+		this.play_local_button = document.getElementById('play_ai');
+		this.play_local_button.addEventListener('click', () => {
+            this.handle_play_ai_button_click();
+        });
+
+		this.remote_game_list_DIV = document.getElementById('remote_game_list');
+
+		this.ws.onopen = (e) => this.send_initial_data_to_server(e);
+        this.ws.onmessage = (e) => this.handle_message(e);
+    }
+
+	send_initial_data_to_server(e) {
+		console.log('WebSocket connection established');
+		ws.send(JSON.stringify({type: 'username', 'username': this.username}));
+		ws.send(JSON.stringify({type: 'lobby_update', 'action': 'display'}));
+	}
+
+    handle_message(e) {
+        try {
+            const data = JSON.parse(e.data);
+			switch (data.type) {
+				case 'lobby_update':
+					this.update_lobby(data);
+					break;
+				case 'join':
+					this.join_game(data.modus);
+					break;
+				default:
+					console.log('Unknown message', data);
+			}
+        } catch (error) {
+            console.log('Error parsing JSON:', error);
+        }
+    }
+
+	handle_create_game_button_click() {
+		console.log("Create game button clicked");
+		ws.send(JSON.stringify({type: 'lobby_update', 'action': 'create',
+			'username': this.username}));
+	}
+
+	handle_play_local_button_click() {
+		console.log("Local game button clicked");
+		this.ws.send(JSON.stringify({type: 'lobby_update', 'action': 'join', 
+			'username': this.username, 'modus': 'local'}))
+	}
+
+	handle_play_ai_button_click () {
+		console.log("AI Opponent button clicked");
+		ws.send(JSON.stringify({type: 'lobby_update', 'action': 'join',
+			'username': this.username, 'modus': 'ai'}));
+	}
+
+	update_lobby(data) {
+		let matches_info = data.matches_info;
+
+		//clear remote_game_list div
+		this.remote_game_list_DIV.innerHTML = '';
+
+		for (var match_id in matches_info) {
+			((id) => {
+				var registered_users = matches_info[id];
+		
+				var matchElement = document.createElement('div');
+				matchElement.innerHTML = `
+					<h2>Match ID: ${id}</h2>
+					<p>Registered Users: ${registered_users.join(', ')}</p>
+					<button class="join" data-match-id="${id}">Join Game</button>
+				`;
+		
+				this.remote_game_list_DIV.appendChild(matchElement);
+		
+				let joinButton = matchElement.querySelector('.join');
+				joinButton.addEventListener('click', () => {
+					console.log("Join button clicked for match id: ", id);
+					this.ws.send(JSON.stringify({type: 'lobby_update', 'action': 'join',
+						'match_id': id, 'username': this.username, 'modus': 'remote'}));
+				});
+			})(match_id);
 		}
-		if (data.type === "join") {
-			ws.modus =  data.modus
+	}
+
+	join_game(modus) {
+		ws.modus = modus;
 			fetch('/pong_online')
 				.then(response => response.text())
 				.then(data => {
@@ -113,83 +140,7 @@ ws.onmessage = function(e) {
 				.catch((error) => {
 					console.error('Error:', error);
 				});
-		}
-		
-	} catch (error) {
-		console.log('Error parsing JSON:', error);
-	}
-};
-
-function updateLobby(data) {
-	matches_info = data.matches_info
-
-	if ('error' in data) {
-		alert(data.error);
-	}
-    // Clear the current lobby
-	console.log("matches:", matches_info)
-    var lobby = document.getElementById('lobby');
-    lobby.innerHTML = '';
-
-	// Add the "Create game" button to the lobby
-	var createGameButton = document.createElement('button');
-	createGameButton.innerText = 'create Game';
-	createGameButton.className = 'create';
-	lobby.appendChild(createGameButton);
-
-	createGameButton.addEventListener('click', function() {
-		console.log("Create game button clicked");
-		ws.send(JSON.stringify({type: 'lobby_update', 'action': 'create', 'username': username}));
-	});
-
-	var localGameButton = document.createElement('button');
-	localGameButton.innerText = 'Local Game';
-	localGameButton.className = 'local';
-	lobby.appendChild(localGameButton);
-
-	localGameButton.addEventListener('click', function() {
-		console.log("Local game button clicked");
-		ws.send(JSON.stringify({type: 'lobby_update', 'action': 'join', 'username': username, 'modus': 'local'}));
-	});
-
-	var aiGameButton = document.createElement('button');
-	aiGameButton.innerText = 'AI Opponent';
-	aiGameButton.className = 'ai';
-	lobby.appendChild(aiGameButton);
-
-	aiGameButton.addEventListener('click', function() {
-		console.log("AI Opponent button clicked");
-		ws.send(JSON.stringify({type: 'lobby_update', 'action': 'join', 'username': username, 'modus': 'ai'}));
-	});
-	
-
-
-   // Add the new match data to the lobby
-   for (var match_id in matches_info) {
-	(function(id) {
-		var registered_users = matches_info[id];
-
-		var matchElement = document.createElement('div');
-		matchElement.innerHTML = `
-			<h2>Match ID: ${id}</h2>
-			<p>Registered Users: ${registered_users.join(', ')}</p>
-			<button class="join" data-match-id="${id}">Join Game</button>
-		`;
-
-		lobby.appendChild(matchElement);
-
-		// New code to add event listeners to the buttons
-		// let registerButton = matchElement.querySelector('.register');
-		// registerButton.addEventListener('click', function() {
-		// 	console.log("Register button clicked for match id: ", id);
-		// 	ws.send(JSON.stringify({type: 'lobby_update', 'action': 'register', 'match_id': id, 'username': username}));
-		// });
-
-		let joinButton = matchElement.querySelector('.join');
-		joinButton.addEventListener('click', function() {
-			console.log("Join button clicked for match id: ", id);
-			ws.send(JSON.stringify({type: 'lobby_update', 'action': 'join', 'match_id': id, 'username': username, 'modus': 'remote'}));
-		});
-	})(match_id);
 	}
 }
+
+const lobby = new Lobby(ws, username);
