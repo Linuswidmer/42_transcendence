4b36d1eff5b218e3e56d0dca5314f4a922d25abb
Marie Darbois <119587916+mdarbois@users.noreply.github.com>
2024-04-20 10:55:08 +0200

    sauber fix, you can play again

--- srcs/app_server/pong/urls.py
+++ srcs/app_server/pong/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('game/', views.game, name='game'),
     path('ws_test/', views.ws_test, name='ws_test'),
     path('landing_page/', views.landing_page, name='landing_page'),
-    
+
     path("sections/<str:name>", views.section, name='section'),
 ]
