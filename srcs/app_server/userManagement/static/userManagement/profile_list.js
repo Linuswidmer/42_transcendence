ded876a958af29118d141517fc5234d73a9d4725
Leon <leon.yannik@web.de>
2024-04-28 15:58:18 +0200

    profile list custom element

---
+++ srcs/app_server/userManagement/static/userManagement/profile_list.js
@@ -0,0 +1,22 @@
+import {router} from "./main.js"
+
+class ProfileList extends HTMLElement {
+    constructor() {
+        super();
+		console.log("constructor profile list custom element");
+
+		document.querySelectorAll(".profileBtn").forEach(button => {
+			button.addEventListener("click", function() {
+				console.log("profileOtherBtn clicked");
+				// Fetch the profile page for the specific user associated with the clicked button
+				const username = this.dataset.username; // Retrieve username from data attribute
+				console.log(username);
+				const url = `/profile/${username}/`; // Construct the URL with the username
+				history.pushState("", "", url);
+				router();
+		 	});
+		});
+    }
+}
+
+customElements.define("pong-list", ProfileList);
