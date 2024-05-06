d55074476740d79b7ab1d1636acd351a2f681f2b
Linus Widmer <lwidmer@student.42berlin.de>
2024-05-06 18:21:28 +0200

    deleted old views

--- srcs/app_server/pong_online/urls.py
+++ srcs/app_server/pong_online/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import pong_online, lobby, register_player, join, display_tournaments
+from .views import pong_online, lobby, display_tournaments
 from userManagement.views import index
 
 app_name = "pong_online"
@@ -8,9 +8,6 @@ urlpatterns = [
     path("fetch/pong_online/", pong_online, name="pong_online"),
 	path("fetch/lobby/", lobby, name="lobby"),
 	
-	
-	path('register_player/', register_player, name='register_player'),
-    path('join/', join, name='join'),
 	path('tournament/<str:tournament_id>/', index, name='display_tournaments'),
 	path('fetch/tournament/<str:tournament_id>/', display_tournaments, name='fetch_display_tournaments')
 ]
--- srcs/app_server/pong_online/views.py
+++ srcs/app_server/pong_online/views.py
@@ -24,38 +24,3 @@ def display_tournaments(request, tournament_id):
 	#print('display ', tournament_id)
 	tournament = lobby.get_tournament(tournament_id)
 	return render(request, "pong_online/tournament.html", {"tournament": tournament}, {"username": request.user.username})
-
-@csrf_exempt
-def register_player(request):
-	if request.method == 'POST':
-		data = json.loads(request.body)
-		match_id = data.get('match_id')
-
-		if match_id is None:
-			return JsonResponse({'status': 'error', 'error_message': 'No match_id provided'})
-
-	# Register user for match
-	lobby = Lobby()
-	match = lobby.get_match(int(match_id))
-	if not match:
-		return JsonResponse({'status': 'error', 'error_message': 'match does not exist'})
-	if not (match.register_player("SESSION ID HERE?")):
-		return JsonResponse({'status': 'error', 'error_message': 'match full'})
-	return JsonResponse({'status': 'success'})
-
-@csrf_exempt
-def join(request):
-	if request.method == 'POST':
-		data = json.loads(request.body)
-		match_id = data.get('match_id')
-
-		if match_id is None:
-			return JsonResponse({'status': 'error', 'error_message': 'No match_id provided'})
-	# Join match
-	lobby = Lobby()
-	match = lobby.get_match(int(match_id))
-	if "SESSION ID HERE?" not in  match.get_registered_players():
-		return JsonResponse({'status': 'error', 'error_message': 'you are not registered to this game'})
-	# return JsonResponse({'status': 'success'})
-	
-	return redirect('pong_online:pong_online')
